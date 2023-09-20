4ae883fc86d02b232919db3e637bd2d0bb5bf8c8
JatinVats <vatsjatin804@gmail.com>
2023-09-20 19:22:34 +0530

    gif

---
+++ index.js
@@ -0,0 +1,18 @@
+// var sound = document.createElement('audio')
+// sound.id = 'audio'
+// sound.controls = 'controls'
+// sound.src = 'http://a.tumblr.com/tumblr_leltkjNwWL1qf32t9o1.mp3'
+// sound.type = 'audio/mp3'
+// document.body.appendChild(sound)
+
+// // function playAudio() {
+// //   document.getElementById('audio').play();
+// // }
+
+// // setTimeout("playAudio()", 1000);
+
+
+// setTimeout(function(){
+//     document.getElementById("audio").play();
+    
+//   }, 1000)
